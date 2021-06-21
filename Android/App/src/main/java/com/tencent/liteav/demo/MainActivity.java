2d75e761dc7f84a706e6a2362e485120bacb0475
lanqiiliu <lanqiiliu@tencent.com>
2021-06-21 16:27:25 +0800

    提交Android模块

---
+++ Android/App/src/main/java/com/tencent/liteav/demo/MainActivity.java
@@ -0,0 +1,171 @@
+package com.tencent.liteav.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.blankj.utilcode.util.SPUtils;
+import com.tencent.liteav.debug.GenerateTestUserSig;
+import com.tencent.liteav.liveroom.model.TRTCLiveRoom;
+import com.tencent.liteav.liveroom.model.TRTCLiveRoomCallback;
+import com.tencent.liteav.liveroom.model.TRTCLiveRoomDef;
+import com.tencent.liteav.liveroom.ui.anchor.TCCameraAnchorActivity;
+import com.tencent.liteav.liveroom.ui.audience.TCAudienceActivity;
+import com.tencent.liteav.liveroom.ui.common.utils.TCConstants;
+import com.tencent.liteav.login.model.ProfileManager;
+import com.tencent.liteav.login.model.RoomManager;
+import com.tencent.liteav.login.model.UserModel;
+
+
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TRTCLiveRoom            mTRTCLiveRoom;
+
+    private Toolbar                 mToolbar;
+    private EditText                mRoomIdEt;
+    private TextView                mEnterRoomTv;
+
+    private String                  mSelfUserId;                         //表示当前登录用户的UserID
+    private boolean                 isUseCDNPlay = false;                //用来表示当前是否CDN模式（区别于TRTC模式）
+
+    private final TextWatcher mEditTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!TextUtils.isEmpty(mRoomIdEt.getText().toString())) {
+                mEnterRoomTv.setEnabled(true);
+            } else {
+                mEnterRoomTv.setEnabled(false);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        RoomManager.getInstance().initSdkAppId(GenerateTestUserSig.SDKAPPID);
+    }
+
+    private void initData() {
+        mRoomIdEt.addTextChangedListener(mEditTextWatcher);
+        mSelfUserId = ProfileManager.getInstance().getUserModel().userId;
+        isUseCDNPlay = SPUtils.getInstance().getBoolean(TCConstants.USE_CDN_PLAY, false);
+
+        final UserModel userModel = ProfileManager.getInstance().getUserModel();
+        mTRTCLiveRoom = TRTCLiveRoom.sharedInstance(this);
+        mTRTCLiveRoom.login(GenerateTestUserSig.SDKAPPID, userModel.userId, userModel.userSig, new TRTCLiveRoomDef.TRTCLiveRoomConfig(isUseCDNPlay, "http://3891.liveplay.myqcloud.com/live"), new TRTCLiveRoomCallback.ActionCallback() {
+            @Override
+            public void onCallback(int code, String msg) {
+                if (code == 0) {
+                    mTRTCLiveRoom.setSelfProfile(userModel.userName, userModel.userAvatar, new TRTCLiveRoomCallback.ActionCallback() {
+                        @Override
+                        public void onCallback(int code, String msg) {
+                            if (code == 0) {
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mRoomIdEt = (EditText) findViewById(R.id.et_room_id);
+        mEnterRoomTv = (TextView) findViewById(R.id.tv_enter_room);
+        findViewById(R.id.tv_enter_room).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String roomId = mRoomIdEt.getText().toString().trim();
+                enterRoom(roomId);
+            }
+        });
+
+        findViewById(R.id.btn_create_room).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createRoom();
+            }
+        });
+
+        findViewById(R.id.btn_trtcliveroom_link).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(TCConstants.TRTC_LIVE_ROOM_DOCUMENT_URL));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    /**
+     * 创建房间
+     */
+    private void createRoom() {
+        Intent intent = new Intent(this, TCCameraAnchorActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * 进入房间
+     * @param roomIdStr
+     */
+    private void enterRoom(String roomIdStr) {
+        int roomId;
+        try {
+            roomId = Integer.parseInt(roomIdStr);
+        } catch (Exception e) {
+            roomId = 10000;
+        }
+        final int roomId2 = roomId;
+        mTRTCLiveRoom.getRoomInfos(Collections.singletonList(roomId), new TRTCLiveRoomCallback.RoomInfoCallback() {
+            @Override
+            public void onCallback(int code, String msg, List<TRTCLiveRoomDef.TRTCLiveRoomInfo> list) {
+                if (0 == code && null != list && !list.isEmpty()) {
+                    final TRTCLiveRoomDef.TRTCLiveRoomInfo info = list.get(0);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            gotoAudience(roomId2, info.ownerId, info.ownerName);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void gotoAudience(int roomId, String anchorId, String anchorName) {
+        Intent intent = new Intent(MainActivity.this, TCAudienceActivity.class);
+        intent.putExtra(TCConstants.GROUP_ID, roomId);
+        intent.putExtra(TCConstants.PUSHER_ID, anchorId);
+        intent.putExtra(TCConstants.PUSHER_NAME, anchorName);
+        startActivity(intent);
+    }
+}
