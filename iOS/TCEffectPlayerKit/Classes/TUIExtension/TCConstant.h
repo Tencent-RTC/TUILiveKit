e6cd1ed798b2dbf8006ccf27dbf8b3b8b25abaa1
krabyu <krabyu@tencent.com>
2025-03-24 10:35:51 +0800

    【TUILiveKit】【iOS】adapts to tencent effect player 3.0 SDK

---
+++ iOS/TCEffectPlayerKit/Classes/TUIExtension/TCConstant.h
@@ -0,0 +1,21 @@
+//
+//  TCConstant.h
+//  TCEffectPlayerKit
+//
+//  Created by krabyu on 2024/7/18.
+//
+
+#ifndef TCConstant_h
+#define TCConstant_h
+
+#define KEY_SERVICE_NAME @"TUIEffectPlayerService"
+#define KEY_GET_VIEW @"TCEffectAnimView"
+#define KEY_METHOD_SET_LICENSE @"setLicense"
+#define KEY_METHOD_START_PLAY @"startPlay"
+#define KEY_METHOD_STOP_PLAY @"stopPlay"
+#define KEY_PARAM_PLAY_URL @"playUrl"
+#define KEY_PARAM_VIEW @"view"
+#define KEY_PARAM_LICENSE_URL @"licenseUrl"
+#define KEY_PARAM_LICENSE_KEY @"licenseKey"
+
+#endif /* TCConstant_h */
---
+++ iOS/TCEffectPlayerKit/Classes/TUIExtension/TUIEffectPlayerService.h
@@ -0,0 +1,16 @@
+//
+//  TUIEffectPlayerService.h
+//  TCEffectPlayerKit
+//
+//  Created by krabyu on 2024/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TUIEffectPlayerService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
